70d4adf67f91e3094ef9cf55eb2e2bae524a550e
Nicolas Dowdy <nickspeed@icloud.com>
2026-09-18 15:03:58 -0600

    adds classes needed for event listener

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+import { products } from '../data/products.js';
+document.addEventListener("DOMContentLoaded", function () {
+    const checkoutCountElement = document.querySelector('.header__checkout-count');
+    const cartPreviewElement = document.querySelector(".header__cart-preview");
+    const cartItemsElement = document.querySelector(".header__cart-items");
+    const cartTotalElement = document.querySelector("header__cart-total-amount");
+    const productListElement = document.querySelector(".store__product-list");
+    const toastContainer = document.querySelector(".toast-container");
+})
