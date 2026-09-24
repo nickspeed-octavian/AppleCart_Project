72bc5563a35e2cf8acaa7bf0baea801e6a54f4d9
Nicolas Dowdy <nickspeed@icloud.com>
2026-09-23 18:05:27 -0600

    adds initial render for products

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,32 @@ document.addEventListener("DOMContentLoaded", function () {
     const cartTotalElement = document.querySelector("header__cart-total-amount");
     const productListElement = document.querySelector(".store__product-list");
     const toastContainer = document.querySelector(".toast-container");
-})
+
+
+    // Function to render products
+
+    function renderProducts(products) {
+        productListElement.innerHTML = ''; // Clear existing content
+
+        products.forEach(product => {
+            const productElement = document.createElement('div');
+            productElement.classList.add('store__product');
+            productElement.setAttribute('data-id', product.id)
+
+            productElement.innerHTML = `
+                <img src="${product.image}" alt="${product.name}" class="store__product-image">
+                <h2 class="store__product-name">${product.name}</h2>
+                <p class="store__product-price">$${product.price.toFixed(2)}</p>
+                <p class="store__product-description>${product.description}</p>
+                <button class="store__add-to-cart">Add to Basket</button>
+            `;
+
+            productListElement.appendChild(productElement);
+        });
+
+        attachAddToCartListeners();
+    }
+
+    // Initial render and update
+    renderProducts(products);
+});
